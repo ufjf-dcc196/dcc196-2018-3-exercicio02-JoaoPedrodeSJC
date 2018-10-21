da21043a9dfa229798145f3d2e81855a8300f834
JoaoPSJC <pedrocxm@gmail.com>
2018-10-21 19:38:35 -0300

    Tela principal feita

---
+++ app/src/main/java/dcc196_2018_3/exercicio02_joaopedrodesjc/MainActivity.java
@@ -0,0 +1,13 @@
+package dcc196_2018_3.exercicio02_joaopedrodesjc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
